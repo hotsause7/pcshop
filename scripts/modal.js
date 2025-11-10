6e95cad6f4ce407f279fe55cac19d393e75c6b32
hotsause7 <hotsause7@users.noreply.github.com>
2025-11-11 00:29:44 +0300

    Открытие по кнопке заказать

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,6 +1,7 @@
 const modalBtn = document.querySelector(".assembly__button");
 const modal = document.querySelector(".modal");
 const modalInner = document.querySelector(".modal__inner");
+const orderBtn = document.querySelector(".course__button");
 
 const closeBtn = document.createElement("div");
 closeBtn.textContent = "X";
@@ -8,8 +9,13 @@ closeBtn.classList.add("modal__close-btn");
 
 modalInner.appendChild(closeBtn);
 
-modalBtn.addEventListener("click", () => {
+const openModal = () => {
   modal.style.display = "flex";
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target === modalBtn || e.target === orderBtn) 
+    openModal();
 });
 
 closeBtn.addEventListener("click", (e) => {
